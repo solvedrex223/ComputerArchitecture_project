126687c5fe16a22e2669d51b2d05a0bd4203dfbe
Emiliano Escobedo <emiliano.escobedo@toshibagcs.com>
2024-04-12 18:59:43 -0600

    Initial commit

---
+++ src/components/BarChart.vue
@@ -0,0 +1,38 @@
+<template>
+    <Line id="my-chart-id" :options="chartOptions" :data="chartData" ref="chart_ref"/>
+</template>
+
+<script setup lang="ts">
+    import { CategoryScale, Chart as ChartJS, Legend, LineElement, LinearScale, PointElement, Title, Tooltip } from 'chart.js';
+import { computed, defineComponent } from 'vue';
+import { Line } from 'vue-chartjs';
+
+    ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale)
+    defineComponent({
+        Line,
+    });
+    const props = defineProps({
+        dataSets: {
+            type: Array<number>,
+            required: true
+        },
+        labels: {
+            type: Array<string>,
+            required: true
+        }
+    });
+
+    const chartData = computed(() => {
+        return {
+            datasets: [{
+                data:[...props.dataSets],
+                label: 'Test',
+                borderColor: '#f87979'
+            }],
+            labels:[...props.labels]
+        }
+    });
+    const chartOptions = {
+        responsive: true
+    }
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <BarChart :dataSets="datasets" :labels="labels"/>
+    <v-btn @click.stop="test">Test</v-btn>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const labels = ref([ '', '', '' ]);
+    const datasets= ref([40, 20, 12]);
+    function test(){
+        datasets.value.push(50);
+        labels.value.push('');
+    }
+</script>
